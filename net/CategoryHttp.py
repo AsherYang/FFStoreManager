37271a5072d71efcb427e5b408a0d5d97c4bdc63
ouyangfan <ouyangfan1991@gmail.com>
2018-07-14 00:10:56 +0800

    category http

---
+++ net/CategoryHttp.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python
+# -*- coding:utf-8 -*-
+
+"""
+Author: AsherYang
+Email:  ouyangfan1991@gmail.com
+Date:   2018/7/13
+Desc:   分类网络操作
+"""
+
+
+class CategoryHttp:
+    def __init__(self):
+        pass
+
+    def addCate(self):
+        pass
+
+    def deleteCate(self):
+        pass
+
+    def updateCate(self):
+        pass
