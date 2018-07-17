45e9e830b23fc2509951d1d9c8861f0f15641ee1
ouyangfan <ouyangfan1991@gmail.com>
2018-07-18 00:04:18 +0800

    1. complete adverts http 2. complete category http

--- net/CategoryHttp.py
+++ net/CategoryHttp.py
@@ -12,6 +12,7 @@ from util import HttpUtil
 from net import HttpApi
 from LoginHttp import LoginHttp
 from constant import ResponseCode
+from constant.CategoryShowType import *
 import json
 
 
@@ -33,11 +34,32 @@ class CategoryHttp:
         if body['code'] == ResponseCode.op_success:
             print 'success: ', body['result']
 
-    def deleteCate(self):
-        pass
+    def deleteCate(self, cate_id):
+        loginInfo = self.loginHttp.getLoginInfoDict()
+        if loginInfo is None or loginInfo.keys() is None:
+            return False
+        user_tel = loginInfo.keys()[0]
+        sms_pwd = loginInfo[user_tel]
+        params = {"tel": user_tel, "sms": sms_pwd, "cate_id": cate_id}
+        body = HttpUtil.http_post(HttpApi.HOST_URl + HttpApi.URL_DELETE_CATE, params=params, header={})
+        body = json.loads(body)
+        print 'deleteCate: ', body
+        if body['code'] == ResponseCode.op_success:
+            print 'success: ', body['result']
 
-    def updateCate(self):
-        pass
+    def updateByCateCode(self, cate_code, parent_code, cate_name, cate_logo, show_type=TYPE_SHOW_NORMAL):
+        loginInfo = self.loginHttp.getLoginInfoDict()
+        if loginInfo is None or loginInfo.keys() is None:
+            return False
+        user_tel = loginInfo.keys()[0]
+        sms_pwd = loginInfo[user_tel]
+        params = {"tel": user_tel, "sms": sms_pwd, "catecode": cate_code, "parentcode": parent_code,
+                  "catename": cate_name, "catelogo": cate_logo, "showtype": show_type}
+        body = HttpUtil.http_post(HttpApi.HOST_URl + HttpApi.URL_UPDATE_CATE, params=params, header={})
+        body = json.loads(body)
+        print 'updateCate: ', body
+        if body['code'] == ResponseCode.op_success:
+            print 'success: ', body['result']
 
 
 if __name__ == '__main__':
@@ -45,4 +67,6 @@ if __name__ == '__main__':
     cateHttp = CategoryHttp()
     cateHttp.loginHttp.init_global()
     cateHttp.loginHttp.login(user_tel=u'13553831061', sms_pwd='735431')
-    cateHttp.addCate(u'上衣', '13', '2', 1, 'http://www.baidu.com')
+    # cateHttp.addCate(u'上衣', '12345', '236', 4, 'http://www.baidu.com')
+    # cateHttp.deleteCate('4116946574260572161')
+    cateHttp.updateByCateCode(cate_code='12345', parent_code=3333, cate_name='裤子3', cate_logo='http://www.oyf.name')
