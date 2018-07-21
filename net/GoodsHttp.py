4f9b1f6df010381a9330d676d42816822b79785f
ouyangfan <ouyangfan1991@gmail.com>
2018-07-21 22:22:09 +0800

    1. update goods http

--- net/GoodsHttp.py
+++ net/GoodsHttp.py
@@ -57,8 +57,38 @@ class GoodsHttp:
         if body['code'] == ResponseCode.op_success:
             print 'success: ', body['result']
 
-    def updateGoods(self):
-        pass
+    def updateGoods(self, netGoods):
+        loginInfo = self.loginHttp.getLoginInfoDict()
+        if not netGoods:
+            return False
+        if loginInfo is None or loginInfo.keys() is None:
+            return False
+        user_tel = loginInfo.keys()[0]
+        sms_pwd = loginInfo[user_tel]
+        """
+        cate_id = param['cateid']
+        brand_id = param['brandid']
+        goods_name = param['name']
+        market_price = param['marketprice']
+        current_price = param['currentprice']
+        sale_count = param['salecount']
+        stock_num = param['stocknum']
+        status = param['status']
+        goods_code = param['goodscode']
+        goods_logo = param['goodslogo']
+        thum_logo = param['thumlogo']
+        keywords = param['keywords']
+        """
+        params = {"tel": user_tel, "sms": sms_pwd, "cateid": netGoods.cate_id, "brandid": netGoods.brand_id,
+                  "name": netGoods.goods_name, "marketprice": netGoods.market_price, "salecount": netGoods.sale_count,
+                  "currentprice": netGoods.current_price, "stocknum": netGoods.stock_num, "status": netGoods.status,
+                  "goodscode": netGoods.goods_code, "goodslogo": netGoods.goods_logo, "thumlogo": netGoods.thum_logo,
+                  "keywords": netGoods.keywords}
+        body = HttpUtil.http_post(HttpApi.HOST_URl + HttpApi.URL_UPDATE_GOODS, params=params, header={})
+        body = json.loads(body)
+        print 'updateGoods: ', body
+        if body['code'] == ResponseCode.op_success:
+            print 'success: ', body['result']
 
 if __name__ == '__main__':
     # 所有的接口都必须要求先进行登录
@@ -79,3 +109,4 @@ if __name__ == '__main__':
     netGoods.keywords = u'上衣, 裤子, 黑色'
     # goodsHttp.addGoods(netGoods)
     goodsHttp.deleteGoods('')
+    # goodsHttp.updateGoods(None)
